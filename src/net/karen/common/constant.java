48f16d2db9aca1f0719734ea496befa2f2e4831b
karenxia <33539005+karenxia@users.noreply.github.com>
2017-11-10 14:39:24 +0800

    v1.0

---
+++ src/net/karen/common/constant.java
@@ -0,0 +1,7 @@
+package net.karen.common;
+
+public class constant{
+	public static final String COMMA_DELIMITER = ",";
+	public static final String DOUBLE_QUOTER = "\"";
+	public static final String NEW_LINE_SEPARATOR = "\n";
+}
